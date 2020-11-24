f5f0c4eef09cc1ca6d4774fc876b5ac5a9c7c988
David Fernández Ramos <dfz.ramos@gmail.com>
2020-11-24 18:39:46 +0100

    Add common data models

---
+++ Common.Contracts/Date.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Common.Contracts
+{
+    /// <summary>
+    /// This class contains the implementation of the workshop date data model.
+    /// </summary>
+    public class Date
+    {
+        /// <summary>
+        /// Gets or sets the identifier of the date.
+        /// </summary>
+        public string Id { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the reparation datetime of the date.
+        /// </summary>
+        public DateTime ReparationDate { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the reason of the date.
+        /// </summary>
+        public ReparationType Reason { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the plate number of the car of the date.
+        /// </summary>
+        public string PlateNumber { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the client identifier who requested the date.
+        /// </summary>
+        public string IdClient { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the reparation status related to this date.
+        /// </summary>
+        public ReparationStatus Status { get; set; }
+    }
+}
---
+++ Common.Contracts/Vehicle.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+
+namespace Common.Contracts
+{
+    /// <summary>
+    /// This class contains the definition of the vehicle data model.
+    /// </summary>
+    public class Vehicle
+    {
+        /// <summary>
+        /// Gets or sets the plate number of the vehicle.
+        /// </summary>
+        public string PlateNumber { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the model of the vehicle.
+        /// </summary>
+        public string Model { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the type of the vehicle.
+        /// </summary>
+        public VehicleType VehicleType { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the year of the vehicle.
+        /// </summary>
+        public int Year { get; set; } = DateTime.Today.Year;
+        
+        /// <summary>
+        /// Gets or sets the last TBDS date of the vehicle.
+        /// </summary>
+        public DateTime LastTBDS { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the last ITV date of the vehicle.
+        /// </summary>
+        public DateTime LastITV { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the identifier of the vehicle owner.
+        /// </summary>
+        public string IdClient { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the identifiers of the vehicle additional drivers.
+        /// </summary>
+        public List<string> IdDrivers { get; set; }
+
+        /// <summary>
+        /// Gets a value indicating if this model has all required fields properly filled
+        /// </summary>
+        public bool IsComplete => !string.IsNullOrEmpty(Model) &&
+                                  !string.IsNullOrEmpty(PlateNumber);
+    }
+}
