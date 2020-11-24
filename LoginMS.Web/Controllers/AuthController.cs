a6c5b0d3e6da9987bb1217e4def4471c016ba3f3
David Fernández Ramos <dfz.ramos@gmail.com>
2020-11-24 19:05:47 +0100

    Create API controllers

---
+++ LoginMS.Web/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LoginMS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public AuthController()
+        {
+        }
+    }
+}
---
+++ VikleAPIMS.Web/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace VikleAPIMS.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController
+    {
+        public UserController()
+        {
+        }
+    }
+}
