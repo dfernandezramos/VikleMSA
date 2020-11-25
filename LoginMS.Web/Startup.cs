42024763dc447e43d50553fd3e1005b8718a836c
David Fernández Ramos <dfz.ramos@gmail.com>
2020-11-25 21:09:16 +0100

    Define projects startups files

--- LoginMS.Web/Startup.cs
+++ LoginMS.Web/Startup.cs
@@ -1,7 +1,49 @@
+using Common.Infrastructure.MongoDB;
+using LoginMS.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 namespace LoginMS.Web
 {
     public class Startup
     {
-        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureRepositories(services);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        private void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddSingleton(new MongoDbSettings
+            {
+                ServerConnection = Configuration["ConnectionStrings:MongoDb:ServerConnection"],
+                Database = Configuration["ConnectionStrings:MongoDb:DatabaseName"]
+            });
+            services.AddTransient<ILoginRepository, LoginRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
     }
 }
--- VikleAPIMS.Web/Startup.cs
+++ VikleAPIMS.Web/Startup.cs
@@ -1,7 +1,49 @@
+using Common.Infrastructure.MongoDB;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using VikleAPIMS.Data;
+
 namespace VikleAPIMS.Web
 {
     public class Startup
     {
-        
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ConfigureRepositories(services);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+        }
+
+        private void ConfigureRepositories(IServiceCollection services)
+        {
+            services.AddSingleton(new MongoDbSettings
+            {
+                ServerConnection = Configuration["ConnectionStrings:MongoDb:ServerConnection"],
+                Database = Configuration["ConnectionStrings:MongoDb:DatabaseName"]
+            });
+            services.AddTransient<IVikleRepository, VikleRepository>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
     }
 }
